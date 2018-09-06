7a434d40e0caa1f6e76dad76b0047319b5b3fdaf
John Bulauan <JBulauan@Johns-MacBook-Air.local>
2018-09-06 23:50:33 +1000

    In app UI mock ups

---
+++ Project2/Project2/Project2/View/MainPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Project2.View
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
